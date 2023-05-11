8e310d5fbdd868400c9a595069fe5f9f03d5168b
Pedro Emídio <pedro.emdio@datarain.com.br>
2023-05-10 22:43:14 -0300

    first commit

--- template-app/main/main.c
+++ template-app/main/main.c
@@ -2,7 +2,7 @@
  * Lib C
  */
 #include <stdio.h>
-#include <stdint.h>  
+#include <stdint.h>
 #include <string.h>
 
 /*
@@ -19,48 +19,29 @@
 #include "wifi.h"
 #include "nvs_flash.h"
 #include "esp_random.h"
-#include <dht.h>
-/*
-
-Actividade - due 11/05
-
-Modificar o código para o envio das variaveis de temperatura e humedidade para o envio do
-thinkspeaks.
-*/
-
-#if defined(CONFIG_EXAMPLE_TYPE_DHT11)
-#define SENSOR_TYPE DHT_TYPE_DHT11
-#endif
-#if defined(CONFIG_EXAMPLE_TYPE_AM2301)
-#define SENSOR_TYPE DHT_TYPE_AM2301
-#endif
-#if defined(CONFIG_EXAMPLE_TYPE_SI7021)
-#define SENSOR_TYPE DHT_TYPE_SI7021
-#endif
-
 
 /*
  * logs
  */
 #include "esp_log.h"
 
+static const char *TAG = "MAIN-NAV: ";
 
-static const char * TAG = "MAIN-NAV: ";
-
-void http_Socket ( void * pvParameter );
-void http_SendReceive ( void * pvParameter );
+void http_Socket(void *pvParameter);
+void http_SendReceive(void *pvParameter);
 #define DEGUG 1
 #define pwm_pin 18
 
 #define buzzer_off 0
-#define buzzer_on  230
+#define buzzer_on 230
 
-typedef struct xData {
- 	int sock; 
- 	uint32_t val_teste;
+typedef struct xData
+{
+	int sock;
+	uint32_t val_teste;
 } xSocket_t;
 
-const char * msg_post = \
+const char *msg_post =
 
 	"POST /update HTTP/1.1\n"
 	"Host: api.thingspeak.com\n"
@@ -69,12 +50,11 @@ const char * msg_post = \
 	"Content-Type: application/x-www-form-urlencoded\n"
 	"content-length: ";
 
-
-void http_Socket(void * pvParameter)
+void http_Socket(void *pvParameter)
 {
-	int rc; 
+	int rc;
 	xSocket_t xSocket;
-	
+
 	for (;;)
 	{
 		int sock;
@@ -85,181 +65,97 @@ void http_Socket(void * pvParameter)
 		if (rc == -1)
 		{
 			ESP_LOGI(TAG, "error on Socket: %d", rc);
-			for( int i = 1; i <= 5; ++i )
-			{	
-				ESP_LOGI(TAG, "timeout: %d", 5-i);
-				vTaskDelay( 1000/portTICK_PERIOD_MS );
+			for (int i = 1; i <= 5; ++i)
+			{
+				ESP_LOGI(TAG, "timeout: %d", 5 - i);
+				vTaskDelay(1000 / portTICK_PERIOD_MS);
 			}
-			continue;			
+			continue;
 		}
 
 		xSocket.sock = sock;
 		xSocket.val_teste = 10;
 
-		xTaskCreate( http_SendReceive, "http_SendReceive", 10000, (void*)&(xSocket), 5, NULL );
+		xTaskCreate(http_SendReceive, "http_SendReceive", 10000, (void *)&(xSocket), 5, NULL);
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
 	}
 	vTaskDelete(NULL);
-	
 }
 
-void http_SendReceive(void * pvParameter)
+void http_SendReceive(void *pvParameter)
 {
-	int rec_offset = 0; 
-	int total =	1*1024; 
+	int rec_offset = 0;
+	int total = 1 * 1024;
 	char post_string[200];
 	const char *apikey = &API_WRITE_KEY[0];
 
-	char *buffer = pvPortMalloc( total );
-	if( buffer == NULL ) 
+	char *buffer = pvPortMalloc(total);
+	if (buffer == NULL)
 	{
-		
-		ESP_LOGI(TAG, "pvPortMalloc Error\r\n"); //alocate block of memory from the heap
-		vTaskDelete(NULL); 	  
+
+		ESP_LOGI(TAG, "pvPortMalloc Error\r\n"); // alocate block of memory from the heap
+		vTaskDelete(NULL);
 		return;
-	 }
-    xSocket_t * xSocket = (xSocket_t*) pvParameter;
+	}
+	xSocket_t *xSocket = (xSocket_t *)pvParameter;
 	sprintf(post_string, msg_post, apikey);
 
 	char databody[50];
-  	sprintf( databody, "{%s&field1=%d}", API_WRITE_KEY, xSocket->val_teste); //modificar a estructure xdata
-	sprintf( buffer , "%s%d\r\n\r\n%s\r\n\r\n", post_string, strlen(databody), databody);
-  
-	int rc = send( xSocket->sock, buffer, strlen(buffer), 0 );
+	sprintf(databody, "{%s&field1=%d}", API_WRITE_KEY, xSocket->val_teste); // modificar a estructure xdata
+	sprintf(buffer, "%s%d\r\n\r\n%s\r\n\r\n", post_string, strlen(databody), databody);
+
+	int rc = send(xSocket->sock, buffer, strlen(buffer), 0);
 
 	ESP_LOGI(TAG, "HTTP Enviado? rc: %d", rc);
-	
-	for(;;)
+
+	for (;;)
 	{
-		ssize_t sizeRead = recv(xSocket->sock, buffer+rec_offset, total-rec_offset, 0);
-		
-		if ( sizeRead == -1 ) 
+		ssize_t sizeRead = recv(xSocket->sock, buffer + rec_offset, total - rec_offset, 0);
+
+		if (sizeRead == -1)
 		{
-			ESP_LOGI( TAG, "recv: %d", sizeRead );
+			ESP_LOGI(TAG, "recv: %d", sizeRead);
 		}
 
-		if ( sizeRead == 0 ) 
+		if (sizeRead == 0)
 		{
 			break;
 		}
 
-		if( sizeRead > 0 ) 
-		{	
+		if (sizeRead > 0)
+		{
 			ESP_LOGI(TAG, "Socket: %d - Data read (size: %d) was: %.*s", xSocket->sock, sizeRead, sizeRead, buffer);
-		   
-		   rec_offset += sizeRead;
-		 }
 
-		vTaskDelay( 5/portTICK_PERIOD_MS );
+			rec_offset += sizeRead;
+		}
+
+		vTaskDelay(5 / portTICK_PERIOD_MS);
 	}
-	
+
 	rc = close(xSocket->sock);
-	
-	ESP_LOGI(TAG, "close: rc: %d", rc); 
-	
-	vPortFree( buffer );	
 
-	vTaskDelete(NULL); 	
+	ESP_LOGI(TAG, "close: rc: %d", rc);
+
+	vPortFree(buffer);
+
+	vTaskDelete(NULL);
 }
-
-//configuração do PWM
-
-void pwm_config()
-{
-	//configuração do timer
-	ledc_timer_config_t ledc_timer ={
-		.speed_mode = LEDC_HIGH_SPEED_MODE,
-		.timer_num  = LEDC_TIMER_0,
-		.duty_resolution = LEDC_TIMER_8_BIT, // min =0 / max = 2^8 -1
-		.freq_hz = 50, //t = 1/50
-		.clk_cfg = LEDC_AUTO_CLK
-	};
-	ledc_timer_config(&ledc_timer);
-
-	//configuração do channel
-	ledc_channel_config_t ledc_channel = {
-		.speed_mode = LEDC_HIGH_SPEED_MODE,
-		.channel =    LEDC_CHANNEL_0,
-		.timer_sel = LEDC_TIMER_0,
-		.intr_type = LEDC_INTR_DISABLE,
-		.gpio_num = pwm_pin,
-		.duty = 0,
-		.hpoint = 0
-	};
-	ledc_channel_config(&ledc_channel);
-}
-
-//função do dutty
-void send_pwm()
-{
-	for (;;)
-	{
-		//enviando sinal buzzer ->on
-		ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, buzzer_on);
-		ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
-		vTaskDelay(100/portTICK_PERIOD_MS);
-		ledc_stop(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0,0);
-
-		//enviando sinal buzzer ->off
-		ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, buzzer_off);
-		ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
-		vTaskDelay(100/portTICK_PERIOD_MS);
-		ledc_stop(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0,0);		
-	}
-	
-}
-
-void dht_test(void *pvParameters)
-{
-    float temperature, humidity;
-
-#ifdef CONFIG_EXAMPLE_INTERNAL_PULLUP
-    gpio_set_pull_mode(dht_gpio, GPIO_PULLUP_ONLY);
-#endif
-
-    while (1)
-    {
-        if (dht_read_float_data(SENSOR_TYPE, CONFIG_EXAMPLE_DATA_GPIO, &humidity, &temperature) == ESP_OK)
-            printf("Humidity: %.1f%% Temp: %.1fC\n", humidity, temperature);
-        else
-            printf("Could not read data from sensor\n");
-
-        // If you read the sensor data too often, it will heat up
-        // http://www.kandrsmith.org/RJS/Misc/Hygrometers/dht_sht_how_fast.html
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
-}
-
 
 void app_main(void)
 {
 	esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) 
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES)
 	{
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
 	ESP_ERROR_CHECK(ret);
 
 	wifi_config();
-	pwm_config();
 
-	
-    if( ( xTaskCreate( http_Socket, "http_Socket", 2048, NULL, 5, NULL )) != pdTRUE )
+	if ((xTaskCreate(http_Socket, "http_Socket", 2048, NULL, 5, NULL)) != pdTRUE)
 	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar http_Socket.\n" );	
-		return;		
-	}   
-
-    if( ( xTaskCreate( send_pwm, "send_pwm", 2048, NULL, 5, NULL )) != pdTRUE )
-	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar send_pwm.\n" );	
-		return;		
-	}  
-	
-    if( ( xTaskCreate( dht_test, "dht_test", 2048, NULL, 5, NULL )) != pdTRUE )
-	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar dht_test.\n" );	
-		return;		
-	}  	 
-
+		ESP_LOGI(TAG, "error - nao foi possivel alocar http_Socket.\n");
+		return;
+	}
 }
--- template-app/main/wifi.h
+++ template-app/main/wifi.h
@@ -4,24 +4,33 @@
 /*
 Debug
 */
-#undef  ESP_ERROR_CHECK
-#define ESP_ERROR_CHECK(x)   do { esp_err_t rc = (x); if (rc != ESP_OK) { ESP_LOGE("err", "esp_err_t = %d", rc); assert(0 && #x);} } while(0);
+#undef ESP_ERROR_CHECK
+#define ESP_ERROR_CHECK(x)                         \
+    do                                             \
+    {                                              \
+        esp_err_t rc = (x);                        \
+        if (rc != ESP_OK)                          \
+        {                                          \
+            ESP_LOGE("err", "esp_err_t = %d", rc); \
+            assert(0 && #x);                       \
+        }                                          \
+    } while (0);
 
 /*
-API 
+API
 */
-#define SERVER_IP "184.106.153.149" 
-#define SERVER_PORT 80 
-#define API_WRITE_KEY "4YQSYIDJPSCMDZDM"
+#define SERVER_IP "184.106.153.149"
+#define SERVER_PORT 80
+#define API_WRITE_KEY "OWGGBH2R8OKYOKSL"
 
 /*
 wifi credencials
 */
-#define EXAMPLE_ESP_WIFI_SSID 	"francis5G" 
-#define EXAMPLE_ESP_WIFI_PASS 	"12345678" 
+#define EXAMPLE_ESP_WIFI_SSID "POCO X3 Pro"
+#define EXAMPLE_ESP_WIFI_PASS "pedro123"
 
 /*
 Function avaliable from this module
 */
-extern void wifi_config( void );
-extern void  open_socket(int * sock_var, int * status_var);
+extern void wifi_config(void);
+extern void open_socket(int *sock_var, int *status_var);
